71b245172b3e881c0d499081f1d428cb4146a738
William Cowley <willcowley99@gmail.com>
2024-01-16 12:03:25 -0500

    removed bufferline

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,10 +8,6 @@ return {
   "rebelot/kanagawa.nvim",
 
   {
-    'akinsho/bufferline.nvim',
-    version = "*", dependencies = 'nvim-tree/nvim-web-devicons'
-  },
-  {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" }
   },
